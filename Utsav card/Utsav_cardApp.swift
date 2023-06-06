c8c622c87b242d5e34729c5d4483a0784811de7f
Utsav Patel <utsavpatel00265@gmail.com>
2023-06-06 18:59:08 +0530

    Initial Commit

---
+++ Utsav card/Utsav_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Utsav_cardApp.swift
+//  Utsav card
+//
+//  Created by Utsav Patel on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Utsav_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
